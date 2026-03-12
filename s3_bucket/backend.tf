b58b3848a563dc1ee67dda51200e6ef995a79029
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-12 16:45:01 -0500

    separated the root module

--- s3_bucket/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ochirdorj-terraform-backend-bucket"
-    key = "infra/example_s3_bucket/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
-}
