00475570a49111969d72b8be2ebbe2ab44672aa9
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-12 15:47:35 -0500

    rmt -recursive

--- s3_bucket/versions.tf
+++ s3_bucket/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "~> 1.13.0"
   required_providers {
     aws = {
-        version = "~> 6.10.0"
-        source = "hashicorp/aws"
+      version = "~> 6.10.0"
+      source = "hashicorp/aws"
     }
   }
 }
